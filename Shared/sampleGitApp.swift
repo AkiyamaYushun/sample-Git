8ea019669d1d4be8b220eabd2764b2aad6b74cc0
秋山優駿 <akiyamayuushun@akiyamayuushunnoMacBook-Air.local>
2021-10-31 01:13:23 +0900

    新規プロジェクト作成’
    git@github.com:AkiyamaYushun/sample-Git.git
    
    git commit  -m 新規プロジェクトを作成’

---
+++ Shared/sampleGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleGitApp.swift
+//  Shared
+//
+//  Created by 秋山優駿 on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct sampleGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
